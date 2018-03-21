03ab8169eb6982d969962dedb0ebff3e7eb246e2
Mario Peplinski <mariopeplinski@gmail.com>
2018-03-22 00:03:17 +0100

    complete perceptron method

--- R/nnetr.R
+++ R/nnetr.R
@@ -1,46 +1,57 @@
-data(iris)
-head(iris, n=20)
+euclid_norm <- function(x) {sqrt(sum(x^2))}
 
-iris_sub = iris[1:100, c(1, 3, 5)]
-names(iris_sub) = c("sepal", "petal", "species")
-head(iris_sub)
-
-euclid_norm <- function(x) {sqrt(x %*% x)}
-
-classifier <- function(data, weight, bias) {
+unit_step <- function(data, weight) {
     distances <- vector(length = nrow(data))
-    distances <- apply(data, 1, distance_from_separator, weight, bias)
-    ifelse(distances < 0, -1, +1)
+    distances <- apply(data, 1, distance_from_separator, weight)
+    ifelse(distances < 0, -1, 1)
 }
 
-distance_from_separator <- function(data, weight, bias) {
-    distance <- data %*% weight + bias
+distance_from_separator <- function(data, weight) {
+    distance <- data %*% weight
     distance
 }
 
-perceptron <- function(x, y, learning_rate = 1) {
-    weight <- vector(length = ncol(x))
-    bias <- 0
-    counter <- 0
-    weight_update <- TRUE
-
+new_perceptron <- function(formula, data, learning_rate = 1, activation = unit_step) {
+    if(!is.data.frame(data)) stop("Input data must be dataframe")
+    #if(!is.numeric()) stop("Input must be numeric")
+    mf <- model.frame(formula, data)
+    mm <- model.matrix(formula, mf)
+    y <- as.matrix(model.response(mf))
+    x <- mm
+    w <- vector(length = ncol(mm))
+    c <- 0
+    weight_update <- TRUE    
+    R <- max(apply(x, 1, euclid_norm))
+    
     while (weight_update) {
         weight_update <- FALSE
-        y_classified <- classifier(x, weight, bias)
+        y_classified <- activation(x, w)
         for (i in 1:nrow(x)) {
             if (y[i] != y_classified[i]) {
-                weight <- weight + learning_rate * y[i] * x[i,]
-                bias <- bias + learning_rate * y[i] * x[i]
+                w[-1] <- w[-1] + learning_rate * y[i] * x[i,-1]
+                w[1] <- w[1] + learning_rate * y[i] * R^2
+                c <- c + 1
+                weight_update <- TRUE
             }
         }
     }
-    return(list(w=weight, b=bias, updates=counter))
+    s <- euclid_norm(w)
+    weights <- w[-1]/s
+    bias <- w[1]/s
+    updates <- c
+
+    attr(- w[1] / w[2], "intercept")
+    attr(- w[1] / w[2], "slope")
+    return(structure(list(weights, bias, updates),
+                     class = "perceptron",
+                     intercept = -w[1] / w[3],
+                     slope = -w[2] / w[3]
+                     )
+           )
 }                
 
-x <- cbind(iris_sub$sepal, iris_sub$petal)
-y <- rep(+1, length(iris_sub$species))
-y[x == "setosa"] <- -1
-    
-linear_classifier(x, c(0,0), 1)
 
 
+## print.perceptron
+## summary.perceptron
+## plot.perceptron
